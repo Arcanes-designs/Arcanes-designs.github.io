abc2c3bb50fceb6323643ea867f27273ec5247ad
MikA <14973636+MikA2257@users.noreply.github.com>
2023-03-08 11:56:23 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  alert('Thanks for signing up! We\'ll keep you updated.');
+});
